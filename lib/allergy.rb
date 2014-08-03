d27a27ca4fbe2090446a81a8081bf29982ef3747
kyas123 <kyas>
2014-08-02 17:01:29 -0700

    working. rspec isnt running for some reason.

--- lib/allergy.rb
+++ lib/allergy.rb
@@ -1,12 +1,23 @@
-def allergies
- 	allerg
+def allergies(user_input)
 
 	allergens = { 128 => "dogfood",
-				 64 => "pollen", 
-				 32 => "chocolate", 
+				 64 => "pollen",
+				 32 => "tubetops", 
 				 16 => "marionberrys",
               	 8 => "finals",
 	             4 => "redbooks", 
 	             2 => "dryersheets", 
 	             1 => "socks" }
+ 	
+ 	allergz = []
+
+	  allergens.each do  | points, allergen |	
+	  if  points <= user_input 
+	  allergz << allergen
+	  user_input -= points 
+	end
+  end
+  	allergz
 end
+
+#puts allergies()
--- spec/allergy_spec.rb
+++ spec/allergy_spec.rb
@@ -1,11 +1,12 @@
-require 'rspec'
-require 'allergy'
+require ('rspec')
+require ('allergy')
 
 describe 'allergy' do
 	it('should return a score when you input your allergens')
 	expect allergy(1).to(eq["socks"])
 end
-it('lists and reports multiple allergens')
+it('lists and reports multiple allergens') do
 expect allerrgy(10).to(eq["finals","dryersheets"])
+end
 	
 end
